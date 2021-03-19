ef3aa57b011beaaa31545c311ec7bf515d8e681c
JeffMony <jeffmony@163.com>
2021-03-19 14:08:30 +0800

    解决https的链接证书请求发生的错误
    Signed-off-by: JeffMony <jeffmony@163.com>

--- videocache/src/main/java/com/jeffmony/videocache/utils/HttpUtils.java
+++ videocache/src/main/java/com/jeffmony/videocache/utils/HttpUtils.java
@@ -1,11 +1,23 @@
 package com.jeffmony.videocache.utils;
 
+import android.annotation.SuppressLint;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.NoRouteToHostException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLHandshakeException;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * @author jeffmony
@@ -15,28 +27,42 @@ import java.util.Map;
 
 public class HttpUtils {
 
+    private static final String TAG = "HttpUtils";
     public static final int MAX_RETRY_COUNT = 100;
-    private static final int MAX_REDIRECT = 5;
+    public static final int MAX_REDIRECT = 5;
     public static final int RESPONSE_200 = 200;
     public static final int RESPONSE_206 = 206;
     public static final int RESPONSE_503 = 503;
 
     public static HttpURLConnection getConnection(String videoUrl, Map<String, String> headers) throws IOException {
+        return getConnection(videoUrl, headers, false);
+    }
+
+    private static HttpURLConnection getConnection(String videoUrl, Map<String, String> headers, boolean shouldIgnoreCertErrors) throws IOException {
         URL url = new URL(videoUrl);
         int redirectCount = 0;
         while (redirectCount < MAX_REDIRECT) {
-            HttpURLConnection connection = makeConnection(url, headers);
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_MULT_CHOICE ||
-                    responseCode == HttpURLConnection.HTTP_MOVED_PERM ||
-                    responseCode == HttpURLConnection.HTTP_MOVED_TEMP ||
-                    responseCode == HttpURLConnection.HTTP_SEE_OTHER) {
-                String location = connection.getHeaderField("Location");
-                connection.disconnect();
-                url = handleRedirect(url, location);
-                redirectCount++;
-            } else {
-                return connection;
+            try {
+                HttpURLConnection connection = makeConnection(url, headers, shouldIgnoreCertErrors);
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_MULT_CHOICE ||
+                        responseCode == HttpURLConnection.HTTP_MOVED_PERM ||
+                        responseCode == HttpURLConnection.HTTP_MOVED_TEMP ||
+                        responseCode == HttpURLConnection.HTTP_SEE_OTHER) {
+                    String location = connection.getHeaderField("Location");
+                    connection.disconnect();
+                    url = handleRedirect(url, location);
+                    redirectCount++;
+                } else {
+                    return connection;
+                }
+            } catch (IOException e) {
+                if ((e instanceof SSLHandshakeException || e instanceof SSLPeerUnverifiedException) && !shouldIgnoreCertErrors) {
+                    //这种情况下需要信任证书重试
+                    return getConnection(videoUrl, headers, true);
+                } else {
+                    throw e;
+                }
             }
         }
         throw new NoRouteToHostException("Too many redirects: " + redirectCount);
@@ -55,8 +81,11 @@ public class HttpUtils {
         return url;
     }
 
-    private static HttpURLConnection makeConnection(URL url, Map<String, String> headers) throws IOException {
+    private static HttpURLConnection makeConnection(URL url, Map<String, String> headers, boolean shouldIgnoreCertErrors) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        if (shouldIgnoreCertErrors && connection instanceof HttpsURLConnection) {
+            trustAllCert((HttpsURLConnection) connection);
+        }
         connection.setInstanceFollowRedirects(false);   //因为我们内部已经做了重定向的功能,不需要在connection内部再做了.
         connection.setConnectTimeout(ProxyCacheUtils.getConfig().getConnTimeOut());
         connection.setReadTimeout(ProxyCacheUtils.getConfig().getReadTimeOut());
@@ -67,6 +96,41 @@ public class HttpUtils {
         }
         connection.connect();
         return connection;
+    }
+
+    public static void trustAllCert(HttpsURLConnection httpsURLConnection) {
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            if (sslContext != null) {
+                sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+                    @SuppressLint("TrustAllX509TrustManager")
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    @SuppressLint("TrustAllX509TrustManager")
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }}, null);
+            }
+        } catch (Exception e) {
+            LogUtils.w(TAG,"SSLContext init failed");
+        }
+        // Cannot do ssl checkl.
+        if (sslContext == null) {
+            return;
+        }
+        // Trust the cert.
+        HostnameVerifier hostnameVerifier = (hostname, session) -> true;
+        httpsURLConnection.setHostnameVerifier(hostnameVerifier);
+        httpsURLConnection.setSSLSocketFactory(sslContext.getSocketFactory());
     }
 
     public static void closeConnection(HttpURLConnection connection) {
