21752c0988e62e6dbfb08a5927200b7c48f5c0c5
Wesley <sunbeyond97@163.com>
2024-07-16 21:29:00 +0800

    style:format code

--- videocache/src/main/java/com/jeffmony/videocache/common/VideoCacheConfig.java
+++ videocache/src/main/java/com/jeffmony/videocache/common/VideoCacheConfig.java
@@ -2,12 +2,12 @@ package com.jeffmony.videocache.common;
 
 public class VideoCacheConfig {
 
-    private long mExpireTime;      //video cache中的过期时间，超过过期时间采用LRU清理规则清理video cache数据
-    private long mMaxCacheSize;    //设置video cache最大的缓存限制,例如超过2G，则采用LRU清理规则清理video cache数据
-    private String mFilePath;      //video cache存储的位置
-    private int mReadTimeOut;      //网络读超时
-    private int mConnTimeOut;      //网络建连超时
-    private boolean mIgnoreCert;   //是否忽略证书校验
+    private final long mExpireTime;      //video cache中的过期时间，超过过期时间采用LRU清理规则清理video cache数据
+    private final long mMaxCacheSize;    //设置video cache最大的缓存限制,例如超过2G，则采用LRU清理规则清理video cache数据
+    private final String mFilePath;      //video cache存储的位置
+    private final int mReadTimeOut;      //网络读超时
+    private final int mConnTimeOut;      //网络建连超时
+    private final boolean mIgnoreCert;   //是否忽略证书校验
     private int mPort;             //本地代理的端口
     private boolean mUseOkHttp;    //使用okhttp接管网络请求
 
