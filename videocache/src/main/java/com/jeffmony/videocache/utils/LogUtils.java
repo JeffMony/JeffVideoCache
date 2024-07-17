ec637c26827a6dddfc176cbb2e29d2e63918d52d
Wesley <sunbeyond97@163.com>
2024-07-17 16:35:58 +0800

    feat:优化日志工具

--- videocache/src/main/java/com/jeffmony/videocache/utils/LogUtils.java
+++ videocache/src/main/java/com/jeffmony/videocache/utils/LogUtils.java
@@ -9,6 +9,7 @@ import android.util.Log;
  */
 
 public class LogUtils {
+    private static final String PREFIX = "VideoCache-";
 
     private static boolean DEBUG = false;
     private static boolean INFO = true;
@@ -18,25 +19,25 @@ public class LogUtils {
 
     public static void d(String tag, String msg) {
         if (DEBUG) {
-            Log.d(tag, msg);
+            Log.d(PREFIX + tag, msg);
         }
     }
 
     public static void i(String tag, String msg) {
         if (INFO) {
-            Log.i(tag, msg);
+            Log.i(PREFIX + tag, msg);
         }
     }
 
     public static void w(String tag, String msg) {
         if (WARN) {
-            Log.w(tag, msg);
+            Log.w(PREFIX + tag, msg);
         }
     }
 
     public static void e(String tag, String msg) {
         if (ERROR) {
-            Log.e(tag, msg);
+            Log.e(PREFIX + tag, msg);
         }
     }
 }
